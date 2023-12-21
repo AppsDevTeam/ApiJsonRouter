40f87b2e1e47b59e912a7605dcfd14eadbc2bc68
Tomáš Kudělka <tomas@appsdevteam.com>
2023-12-21 11:19:19 +0100

    Umozneni vice method

--- src/ApiRouteList.php
+++ src/ApiRouteList.php
@@ -10,10 +10,23 @@ class ApiRouteList extends RouteList
 	public function addRoutesBySpecification(array $apiRouteSpecification): self
 	{
 		foreach ($apiRouteSpecification as $route) {
-			$this[] = new ApiRoute($route['path'], $route['presenter'], [
-				'methods' => isset($route['action'])
+			if (is_array($route['method'])) {
+				if (isset($route['action'])) {
+					foreach ($route['method'] as $_method) {
+						$methods[$_method] = $route['action'];
+					}
+
+				} else {
+					$methods = $route['method'];
+				}
+			} else {
+				$methods = isset($route['action'])
 					? [ $route['method'] => $route['action'] ]
-					: [ $route['method'] ],
+					: [ $route['method'] ];
+			}
+
+			$this[] = new ApiRoute($route['path'], $route['presenter'], [
+				'methods' => $methods,
 				'parameters' => $route['parameters'] ?? [],
 			], $route['body'] ?? null);
 		}
