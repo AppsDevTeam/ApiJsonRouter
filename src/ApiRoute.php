af1900fb11c68116eb8a90dcab00a61e634f332a
Viktor Masicek <viktor@appsdevteam.com>
2024-07-09 10:24:55 +0200

    Log used HTTP method on not allowed method (error 405)
    (cherry picked from commit b69c27f7eb208d6bdd41257b8e964da4e1df2c6b)

--- src/ApiRoute.php
+++ src/ApiRoute.php
@@ -113,7 +113,7 @@ class ApiRoute extends \Contributte\ApiRouter\ApiRoute
 	{
 		if (!in_array($request->getMethod(), $this->getMethods(), true)) {
 			throw new ClientException(
-				'Allowed methods: ' . implode(', ', $this->getMethods()) . '.',
+				'Allowed methods: ' . implode(', ', $this->getMethods()) . '. Used method: ' . $request->getMethod() . '.',
 				Nette\Http\IResponse::S405_MethodNotAllowed
 			);
 		}
