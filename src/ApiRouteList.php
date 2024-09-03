128b81cd809b2ae019a4f67f4131651f914078ea
Pavel Konvička <pavel.konvicka@gmail.com>
2024-09-03 22:45:26 +0200

    fix body/parameters for same endpoint

--- src/ApiRouteList.php
+++ src/ApiRouteList.php
@@ -13,12 +13,18 @@ class ApiRouteList extends RouteList
 			if (is_array($route['method'])) {
 				if (isset($route['action'])) {
 					foreach ($route['method'] as $_method) {
-						$methods[$_method] = $route['action'];
+						if (isset($route['action'][$_method])) {
+							$methods[$_method] = $route['action'][$_method];
+						}
+						else {
+							$methods[$_method] = $route['action'];
+						}
 					}
 
 				} else {
 					$methods = $route['method'];
 				}
+
 			} else {
 				$methods = isset($route['action'])
 					? [ $route['method'] => $route['action'] ]
@@ -27,8 +33,8 @@ class ApiRouteList extends RouteList
 
 			$this[] = new ApiRoute($route['path'], $route['presenter'], [
 				'methods' => $methods,
-				'parameters' => $route['parameters'] ?? [],
-			], $route['body'] ?? null);
+				'parameters' => $route['parameters'][$_SERVER['REQUEST_METHOD']] ?? $route['parameters'] ?? [],
+			], $route['body'][$_SERVER['REQUEST_METHOD']] ?? $route['body'] ?? null);
 		}
 
 		return $this;
